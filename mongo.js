0bd6cdb2cbc6aca1489784829d73e22bb28d1c59
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-13 15:12:31 -0300

    c0-adding mongo.js to learning and adding aplication

---
+++ mongo.js
@@ -0,0 +1,29 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+//when a build one const to receive mongoclinet i need adding connect and adding
+//localhost and name of banc
+
+//here i can adding mongoclient i need whote way that url always he receive one promisses
+MongoClient.connect('mongodb://localhost:27017/intro-mongo', (err, db) => {
+   
+  //this i called the collection when a create const to receive ds.collection
+  const pessoas = db.collection('pessoas')
+  //pessoas.insert({
+     // name: 'Felipe programmer',
+     // born:'18-01-1995',
+     // adress:'Avenida circular 921',
+     // complemento: 'casa',
+      //Nacionalidade:'Brasileiro'
+     /* const CursorPessoas = pessoas.find({})
+      CursorPessoas.forEach( doc => {
+          console.log(doc)
+      },() => console.log('end') ) */
+
+
+      
+
+})
+      //, (err, res)=> {
+     //console.log(err, res)
+  //})
+//}
