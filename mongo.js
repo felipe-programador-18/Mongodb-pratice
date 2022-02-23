5b55df21122cdc163104d6e72f36c5bbd574d868
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-23 17:44:20 -0300

    c6- remove date with mongodb

--- mongo.js
+++ mongo.js
@@ -8,30 +8,18 @@ MongoClient.connect('mongodb://localhost:27017/intro-mongo', (err, db) => {
    
   //this i called the collection when a create const to receive ds.collection
   const pessoas = db.collection('pessoas')
-  //pessoas.insert({
-     // name: 'Felipe programmer',
-     // born:'18-01-1995',
-     // adress:'Avenida circular 921',
-     // complemento: 'casa',
-      //Nacionalidade:'Brasileiro'
-     /* const CursorPessoas = pessoas.find({})
-      CursorPessoas.forEach( doc => {
-          console.log(doc)
-      },() => console.log('end') ) */
-
-      //THIS IS TO UPDATE DATABASE
-      //The method have three parameters to update
-      pessoas.update({
-          _id: mongodb.ObjetcID('62029720a78ce03150a9cf0b')
-      }, {
-          $set:{
-              nome:'Felipe Martins',
-              born:'1853-01-30'
-          }
-      }, (err, res) => console.log(err))
+      //pessoas.update(
+        //  {
+          // _id: mongodb.ObjectID('620930b6debd9326a05aec62')
+      //}, {
+        //  $set:{
+          //    nome:'Felipe Martins',
+            //  born:'1853-01-30'
+        //  }
+      //}, (err, res) => console.log(err))
+    pessoas.remove({
+        _id: mongodb.ObjectID('620930b6debd9326a05aec62')
+    }, (err,res) => console.log(err)  )
 
 })
-      //, (err, res)=> {
-     //console.log(err, res)
-  //})
-//}
+  
