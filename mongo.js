c6b577e6ea0d7001f6f2f9835940b8e52ceeb8ce
Felipe <machadofelipe2016@outlook.com>
2022-03-26 20:42:00 -0300

    adding some

--- mongo.js
+++ mongo.js
@@ -4,22 +4,16 @@ const MongoClient = mongodb.MongoClient
 //localhost and name of banc
 
 //here i can adding mongoclient i need whote way that url always he receive one promisses
-MongoClient.connect('mongodb://localhost:27017/intro-mongo', (err, db) => {
+MongoClient.connect('mongodb://localhost:27017/pessoas', (err, db) => {
    
   //this i called the collection when a create const to receive ds.collection
   const pessoas = db.collection('pessoas')
-      //pessoas.update(
-        //  {
-          // _id: mongodb.ObjectID('620930b6debd9326a05aec62')
-      //}, {
-        //  $set:{
-          //    nome:'Felipe Martins',
-            //  born:'1853-01-30'
-        //  }
-      //}, (err, res) => console.log(err))
-    pessoas.remove({
-        _id: mongodb.ObjectID('620930b6debd9326a05aec62')
-    }, (err,res) => console.log(err)  )
-
+  pessoas.insert({
+    name:"Programer 18",
+    age:'30',
+    city:'Porto alegre'
+  }, (err, res) => {
+    console.log(err, res)
+  }) 
 })
   
